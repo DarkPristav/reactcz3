b96c865d1eb35eaacd64022826b19db42df1780b
Petro Atamaniuk <dark.pristav@gmail.com>
2024-06-23 16:29:39 +0200

    3.2.finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [windowSize, setWindowSize] = useState(window.innerWidth)
+
+  const sizeControler = () => {
+    setWindowSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", sizeControler)
+    return () => {
+    window.removeEventListener("resize", sizeControler)
+  }
+  })
+
+  return <div>
+    <h1>Šířka okna</h1>
+    <h3>{windowSize}</h3>
+  </div>
 }
 
-export default App;
+export default App
